ad26b6c9fd3d7e83ae5c52dad890c551b76f29b7
Marlon Arteaga <marlon.arteaga.m@hotmail.com>
2025-07-22 10:22:09 -0500

    add config AndroidBuildConfig

---
+++ buildSrc/src/main/kotlin/AndroidBuildConfig.kt
@@ -0,0 +1,12 @@
+object AndroidBuildConfig {
+    const val composeVersion = "1.5.7"
+    const val compileSdk = 35
+    const val targetSdk = 33
+    const val minSdk = 24
+    const val basePackage = "com.example"
+    const val applicationId = "com.example.dogsapp"
+    const val versionCode = 1
+    const val versionName = "1.0"
+    const val testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    const val requiresOptIn = "-Xopt-in=kotlin.RequiresOptIn"
+}
